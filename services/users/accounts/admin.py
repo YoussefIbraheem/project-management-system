63cfcc68a2ddd6bc55e4afc77610a9d65c8105ce
YoussefIbraheem <youssefm.muhammed@gmail.com>
2026-07-30 00:55:58 +0300

    **Refactor:** Add user registration event publishing and display name logic

--- services/users/accounts/admin.py
+++ services/users/accounts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.db import models, transaction
+from django.forms.fields import Textarea, TextInput
+
+from accounts.events import UserRegisterEvent
+from accounts.publishers import publish_history_event
 
 from .models import User, UserProfile, UserVerification
 
@@ -18,6 +23,25 @@ class UserAdmin(BaseUserAdmin):
         "date_joined",
         "is_verified",
     ]
+
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": [
+                    "username",
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "usable_password",
+                    "password1",
+                    "password2",
+                ],
+            },
+        ),
+    ]
+
     fieldsets = [
         (
             None,
@@ -29,13 +53,32 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
         ("Personal Data", {"fields": ["first_name", "last_name"]}),
-        ("Advanced Options", {"fields": ["is_verified"]}),
+        ("Advanced Options", {"fields": ["is_verified", "is_superuser"]}),
         ("General Data", {"fields": ["date_joined"]}),
         ("Danger Zone", {"classes": ["collapsable"], "fields": ["password"]}),
     ]
+
     def save_model(self, request, obj, form, change):
-        obj._actor_id = request.user.id
-        return super().save_model(request, obj, form, change)
+        super().save_model(request, obj, form, change)
+        if not change:
+            display_name = ""
+
+            if obj.first_name:
+                display_name += obj.first_name
+
+            if obj.last_name:
+                display_name += " " + obj.last_name
+
+            UserProfile.objects.create(user=obj)
+
+            event = UserRegisterEvent(
+                actor_id=str(request.user.id),
+                subject_id=str(obj.id),
+                username=obj.username,
+                email=obj.email,
+                display_name=display_name,
+            )
+            transaction.on_commit(lambda: publish_history_event(event.to_dict()))
 
 
 class UserProfileAdmin(admin.ModelAdmin):
